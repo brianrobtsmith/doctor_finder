9c5a0dd0d1431d80e9faf7d21e7e6ab32b86da12
Brian Smith <smith.brianr@gmail.com>
2013-10-20 17:11:50 -0500

    Put my website onto the Internets
    On my way to creating something wonderful!  Feels good to create!

---
+++ doctor_finder/app/controllers/killerapp_controller.rb
@@ -0,0 +1,8 @@
+class KillerappController < ApplicationController
+  def docsaver_home
+    render 'docsaver_home'
+  end
+  def docsaver_appointments
+    render 'docsaver_appointments'
+  end
+end
